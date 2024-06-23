6b74adb4766771d6847b761e04420037ed2cbf11
Delon Borges <delon.borges@icloud.com>
2024-06-23 17:27:35 -0300

    feat: first commit

---
+++ src/main/java/com/delon/geracaofaturascartaocreditojob/GeracaoFaturasCartaoCreditoJobApplication.java
@@ -0,0 +1,13 @@
+package com.delon.geracaofaturascartaocreditojob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeracaoFaturasCartaoCreditoJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeracaoFaturasCartaoCreditoJobApplication.class, args);
+    }
+
+}
